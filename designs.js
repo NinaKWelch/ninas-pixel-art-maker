f088a38b59b530a159639f89bee7cd9077876aaf
NinaW <ninakwelch@gmail.com>
2018-02-05 15:24:31 +0000

    JavaScript code added

--- designs.js
+++ designs.js
@@ -1,10 +1,37 @@
 // Select color input
-// Select size input
+
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+    // Select size input
+    var height = $('#input_height').val();
+    var width = $('#input_height').val();
+    var table = $('#pixel_canvas');
 
-// Your code goes here!
+    // Reset table with new submit
+	table.empty();
+
+    // Add rows (tr) to table based on the heigh value
+	for (i = 0; i < height; i++) {
+	table.append('<tr></tr>');
+	}
+
+    // Add cells (td) to table rows (tr) based on the width value
+	for (j = 0; j < width; j++) {
+	$('tr').append('<td></td>');
+	}
+
+	// Select color input
+	table.on('click', 'td', function() {
+		var color = $("#colorPicker").val();
+		$(this).attr('bgcolor', color);
+	});
 
 }
+
+// Stop the page from refreshing on each submit
+$('input[type="submit"]').click(function(event) {
+  event.preventDefault();
+  makeGrid();
+});
