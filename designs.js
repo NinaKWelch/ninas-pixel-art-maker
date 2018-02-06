0f21cb4a41b8c9084a33f64480633fd3a57a20f3
NinaW <ninakwelch@gmail.com>
2018-02-06 12:24:34 +0000

    code and styling

--- designs.js
+++ designs.js
@@ -1,37 +1,39 @@
-// Select color input
-
-
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
     // Select size input
-    var height = $('#input_height').val();
-    var width = $('#input_height').val();
-    var table = $('#pixel_canvas');
+    const TABLE = $('#pixel_canvas');
+    const TABLE_HEIGHT = $('#input_height').val();
+    const TABLE_WIDTH = $('#input_width').val();
+
 
     // Reset table with new submit
-	table.empty();
+	TABLE.empty();
 
     // Add rows (tr) to table based on the heigh value
-	for (i = 0; i < height; i++) {
-	table.append('<tr></tr>');
+	for (let i = 0; i < TABLE_HEIGHT; i++) {
+	    TABLE.append('<tr></tr>');
 	}
 
-    // Add cells (td) to table rows (tr) based on the width value
-	for (j = 0; j < width; j++) {
-	$('tr').append('<td></td>');
+    // Add cells (td) to rows based on the width value
+	for (let j = 0; j < TABLE_WIDTH; j++) {
+	    $('tr').append('<td></td>');
 	}
 
 	// Select color input
-	table.on('click', 'td', function() {
-		var color = $("#colorPicker").val();
-		$(this).attr('bgcolor', color);
+	TABLE.on('click', 'td', function() {
+		let cellColor = $("#colorPicker").val();
+		$(this).attr('bgcolor', cellColor);
 	});
 
 }
 
 // Stop the page from refreshing on each submit
 $('input[type="submit"]').click(function(event) {
-  event.preventDefault();
-  makeGrid();
+    event.preventDefault();
+    // remove placeholder img and h2 on click
+    $('div.content img').css('display', 'none');
+    $('div.content h3').css('display', 'none');
+    makeGrid();
 });
+
