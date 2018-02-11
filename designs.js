4ebbbf936bd8cef9277e4cf72d65ff85f51f0ad2
NinaW <ninakwelch@gmail.com>
2018-02-11 11:07:09 +0000

    New Version

--- designs.js
+++ designs.js
@@ -8,32 +8,42 @@ function makeGrid() {
 
 
     // Reset table with new submit
-	TABLE.empty();
+      TABLE.empty();
 
     // Add rows (tr) to table based on the heigh value
-	for (let i = 0; i < TABLE_HEIGHT; i++) {
-	    TABLE.append('<tr></tr>');
-	}
+      for (let i = 0; i < TABLE_HEIGHT; i++) {
+          TABLE.append('<tr></tr>');
+      }
 
     // Add cells (td) to rows based on the width value
-	for (let j = 0; j < TABLE_WIDTH; j++) {
-	    $('tr').append('<td></td>');
-	}
+      for (let j = 0; j < TABLE_WIDTH; j++) {
+          $('tr').append('<td></td>');
+      }
 
-	// Select color input
-	TABLE.on('click', 'td', function() {
-		let cellColor = $("#colorPicker").val();
-		$(this).attr('bgcolor', cellColor);
-	});
+    // Select color input
+    TABLE.on('mousedown', 'td', function() {
+        let cellColor = $("#colorPicker").val();
+        $(this).css('background-color', cellColor);
+        TABLE.on('mouseover', 'td', function() {
+            $(this).css('background-color', cellColor);
+            TABLE.on('mouseup', function() {
+                TABLE.off('mouseover');
+            });
+        });
+    });
 
+
+    // Remove color on double click
+    TABLE.on('dblclick', 'td', function() {
+        $(this).css('background-color', '');
+    });
 }
 
 // Stop the page from refreshing on each submit
 $('input[type="submit"]').click(function(event) {
     event.preventDefault();
-    // remove placeholder img and h2 on click
+    // remove placeholder img and h3 on click
     $('div.content img').css('display', 'none');
     $('div.content h3').css('display', 'none');
     makeGrid();
 });
-
